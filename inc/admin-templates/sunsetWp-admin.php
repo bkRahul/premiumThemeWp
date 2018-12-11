e57f7f37e8b1cfa57ce15e8e8eed0d191deeb17b
bkRahul <rahulsatya1068@gmail.com>
2018-12-11 23:58:27 +0530

    Added custom fields and sections in the theme admin page

--- inc/admin-templates/sunsetWp-admin.php
+++ inc/admin-templates/sunsetWp-admin.php
@@ -1,4 +1,15 @@
 <h1>Sunset Theme Options</h1>
 <h3 class="title">Manage Options</h3>
-<p>Customize the default Wordpress Options</p>
-<?php bloginfo('name'); ?>
+<p>Customize the sunsetWp Theme Options</p>
+
+<?php settings_errors(); ?>
+
+<form method="POST" action="options.php">
+	
+<?php settings_fields('sunsetWp-settings-group'); 	//Call the settings field on the page 	?>
+
+<?php do_settings_sections('premium_sunsetWp');//(page-url to be called on) 	//Call the settings section on the page 	?>
+
+<?php submit_button(); ?>
+
+</form>
--- inc/function-admin.php
+++ inc/function-admin.php
@@ -16,22 +16,44 @@ function sunsetWp_add_admin_page() {
 
 
 //Generate sunset-theme admin sub-pages 
-	add_submenu_page('premium_sunsetWp', 'Sunset Theme Options', 'General Info', 'manage_options', 'premium_sunsetWp', 'sunsetWp_theme_create_page');
+	add_submenu_page('premium_sunsetWp', 'Sunset Theme Options', 'General Info', 'manage_options', 'premium_sunsetWp', 'sunsetWp_theme_create_page');//(id, title, fieldname, admin privileges, page-url, callback)
 
-	add_submenu_page('premium_sunsetWp', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'premium_sunsetWp_css', 'sunsetWp_theme_settings_page');
+	add_submenu_page('premium_sunsetWp', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'premium_sunsetWp_css', 'sunsetWp_theme_settings_page');//(id, title, fieldname, admin privileges, page-url, callback)
+
+//Activate Custom Settings fields
+	add_action('admin_init', 'sunsetWp_custom_settings') ;	
 
 }
 
 add_action('admin_menu', 'sunsetWp_add_admin_page');
 
+	//Generate Custom sunsetWp-theme admin General Info page 
 function sunsetWp_theme_create_page() {
-	//Generate sunset-theme admin page 
 	require_once(get_template_directory().'/inc/admin-templates/sunsetWp-admin.php');
 }
 
+	//Generate sunsetWp-theme css page
 function sunsetWp_theme_settings_page() {
-	//Generate sunset-theme admin page
 	echo "<h1>Sunset Custom CSS</h1>"; 
 }
 
+function sunsetWp_custom_settings() {
+	//Register Settings fields
+	register_setting('sunsetWp-settings-group', 'first_name');
+
+	//Add Settings Section
+	add_settings_section('sunsetWp-sidebar-options', 'Sidebar Options', 'sunsetWp_sidebar_options', 'premium_sunsetWp'); //(section-id, title, fieldname, callback, page-id)
+	
+	//
+	add_settings_field('sidebar_name', 'First Name', 'sunsetWp_sidebar_name', 'premium_sunsetWp', 'sunsetWp-sidebar-options');//(id, title, callback, page-id, section-id);
+}
+
+function sunsetWp_sidebar_options(){
+	echo "Sunset Sidebar Options";
+}
+
+function sunsetWp_sidebar_name(){
+	$firstname = esc_attr(get_option('first_name'));
+	echo '<input type="text" name="first_name" value="'.$firstname.'"></input>';
+}
 ?>
