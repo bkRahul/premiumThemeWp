13f8e6772e73fa662ca08f3560d158dcbf8b7ceb
bkRahul <rahulsatya1068@gmail.com>
2018-12-15 01:21:25 +0530

    added fields for admin profile picture

--- inc/admin-templates/sunsetWp-admin.php
+++ inc/admin-templates/sunsetWp-admin.php
@@ -1,10 +1,10 @@
 <?php settings_errors(); ?>
 <h1>Sunset Theme Options</h1>
 
-
-
 <?php
-
+	
+	//Store the field variables for use
+ 	$picture = esc_attr(get_option('profile_picture'));
  	$firstname = esc_attr(get_option('first_name'));
 	$lastname = esc_attr(get_option('last_name'));
 
@@ -17,8 +17,13 @@
 <div class="container box-container">
 
 <div class="sidebar-preview">
+	<div class="image-container">
+		<div id="profile-image-preview" class="profile_picture" style="background-image: url(<?php print $picture ?>);"></div>
+	</div>
+ 
+
 	<h1 class="admin-name"><?php print $fullname; ?></h1>
-	<h2 class="admin-dscription"><?php print $description; ?></h2>
+	<h3 class="admin-dscription"><?php print $description; ?></h3>
 	<div class="admin-icons"></div>
 </div>
 
