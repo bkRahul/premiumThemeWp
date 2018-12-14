184ae2bb3993a9fa24d898bb018d5a79330dc55b
bkRahul <rahulsatya1068@gmail.com>
2018-12-15 01:19:41 +0530

    added support for media uploader

--- inc/admin-enqueue.php
+++ inc/admin-enqueue.php
@@ -16,6 +16,11 @@ if('toplevel_page_premium_sunsetWp' != $hook) {
 //css
 	wp_register_style('sunsetWp_admin', get_template_directory_uri().'/css/sunsetWp-admin.css', array(), '1.0.0', 'all');
 	wp_enqueue_style('sunsetWp_admin');
+
+	wp_enqueue_media();// 	call the media uploader
+
+	wp_register_script('sunsetWp_admin', get_template_directory_uri().'/js/sunsetWp-admin.js', array('jquery'), '1.0.0', true);
+	wp_enqueue_script('sunsetWp_admin');
 }
 
 
