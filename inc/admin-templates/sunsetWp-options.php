1cbc6561008dd51d82634d5ccd504e1b8c7c889d
bkRahul <rahulsatya1068@gmail.com>
2018-12-18 23:56:18 +0530

    Custom template for theme options

---
+++ inc/admin-templates/sunsetWp-options.php
@@ -0,0 +1,16 @@
+<?php settings_errors(); ?>
+<h1>Sunset Theme Options</h1>
+
+<div class="container box-container">
+
+<form method="POST" action="options.php" class="sidebar-admin_form">
+	
+<?php settings_fields('sunsetWp-theme-support'); 	//Call the settings field on the page 	?>
+
+<?php do_settings_sections('sunsetWp_options');//(page-url to be called on) 	//Call the settings section on the page 	?>
+
+<?php submit_button(); ?>
+
+</form>
+
+</div>
