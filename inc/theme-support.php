12b252bc725f1b1c96fcc819808d32ff933ea575
bkRahul <rahulsatya1068@gmail.com>
2018-12-19 23:57:25 +0530

    included theme support for header and background

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -1,6 +1,10 @@
 <?php
 
- 	$options = get_option('post-formats');
+ 	$options = get_option('post_formats');
+
+	$background = get_option('custom_background');
+
+	$header = get_option('custom_header');
 
 	$formats = array('aside','image', 'video', 'gallery', 'link', 'quote', 'status', 'audio', 'chat');
 	
@@ -15,6 +19,18 @@
 	if(!empty($options)) {
 
 	add_theme_support('post-formats', $output);
+	
+	}
+
+	if(!empty($background)) {
+
+	add_theme_support('custom-background');
+	
+	}
+
+	if(!empty($header)) {
+
+	add_theme_support('custom-header');
 	
 	}
 
