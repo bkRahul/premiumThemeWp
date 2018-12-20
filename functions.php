292232131439d1b1e097d59c701ddbf1a6b2560b
bkRahul <rahulsatya1068@gmail.com>
2018-12-20 23:57:57 +0530

    enqueued the custom post file

--- functions.php
+++ functions.php
@@ -3,5 +3,5 @@
 require get_template_directory(). '/inc/function-admin.php'; 
 require get_template_directory(). '/inc/admin-enqueue.php'; 
 require get_template_directory(). '/inc/theme-support.php'; 
-
+require get_template_directory(). '/inc/custom-posts.php'; 
 ?>
