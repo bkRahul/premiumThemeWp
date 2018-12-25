e19ef172dce1bf05c81b4fcfff670a7169dd8762
bkRahul <rahulsatya1068@gmail.com>
2018-12-25 23:43:30 +0530

    changed file name to functions-admin.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 
-require get_template_directory(). '/inc/function-admin.php'; 
+require get_template_directory(). '/inc/functions-admin.php'; 
 require get_template_directory(). '/inc/admin-enqueue.php'; 
 require get_template_directory(). '/inc/theme-support.php'; 
 require get_template_directory(). '/inc/custom-posts.php'; 
