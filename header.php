b297e9d7f74ad36e8ec10c17bf9a4ef5978faed7
bkRahul <rahulsatya1068@gmail.com>
2018-12-28 00:02:03 +0530

    included all the necessary dependencies in the header

--- header.php
+++ header.php
@@ -1,1 +1,48 @@
-<?php wp_head() ?>
+<?php 
+/*
+
+
+This is the template for the header
+
+@package SunsetWp
+
+
+*/
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<title><?php bloginfo('name'); ?><?php wp_title('|') ?></title>
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+<?php if (is_singular() && pings_open(get_queried_object())	)	: ?>
+	<link rel="pingback" href="<?php bloginfo('pingback'); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12">
+
+				<div class="header-container " style="background-image: url(<?php header_image(); ?>)">
+
+					<div class="header-content text-center">
+						<h1 class="site-title"><?php bloginfo('name'); ?></h1>
+						<h3 class="site-description"><?php bloginfo('description'); ?></h3>
+					</div><!-- .header-content -->
+
+					<div class="header-nav">
+						
+					</div><!-- .header-nav -->
+
+				</div><!-- .header-container -->
+
+			</div><!-- .col-xs-12 -->
+		</div><!-- .row -->
+	</div><!-- .container-fluid -->
