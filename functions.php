79a8e5164a9ce98e23780f4537761a75f7eb0d56
bkRahul <rahulsatya1068@gmail.com>
2019-01-02 23:52:41 +0530

    enqueued cleanup file

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@
 require get_template_directory(). '/inc/functions-admin.php'; 
 require get_template_directory(). '/inc/scripts-enqueue.php'; 
 require get_template_directory(). '/inc/theme-support.php'; 
-require get_template_directory(). '/inc/custom-posts.php'; 
+require get_template_directory(). '/inc/custom-posts.php';
+require get_template_directory(). '/inc/cleanup.php';
 ?>
