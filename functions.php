461c9a06e075e573714480fdf4f93eb35c720ef8
bkRahul <rahulsatya1068@gmail.com>
2019-01-07 00:01:49 +0530

    enqueued walker file

--- functions.php
+++ functions.php
@@ -5,4 +5,5 @@ require get_template_directory(). '/inc/scripts-enqueue.php';
 require get_template_directory(). '/inc/theme-support.php'; 
 require get_template_directory(). '/inc/custom-posts.php';
 require get_template_directory(). '/inc/cleanup.php';
+require get_template_directory(). '/inc/walker.php';
 ?>
