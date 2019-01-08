4104d842fb11ce213b15e3e5ab0172dae000196a
bkRahul <rahulsatya1068@gmail.com>
2019-01-09 00:01:23 +0530

    template parts folder containing custom content templates

---
+++ template-parts/content.php
@@ -0,0 +1,40 @@
+<?php 
+/*
+
+
+This is the template for the standard post content
+
+@package SunsetWp
+
+
+*/
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
+	<header class="entry-header">
+		<?php the_title('<h1 class="entry-title">', '</h1>') ?>
+		<div class="entry-meta">
+			<?php sunsetWp_posts_meta(); ?>
+		</div><!-- .entry-meta -->
+	</header>
+
+	<div class="entry-content">
+		<?php if( has_post_thumbnail() ): ?>
+			<div class="standard-featured-image">
+				<?php the_post_thumbnail(); ?>
+			</div>
+		<?php endif ?>
+
+		<div class="entry-excerpt">
+			<?php the_excerpt(); ?>
+		</div>
+
+		<div class="button-container">
+			<a href="<?php the_permalink(); ?>" class="btn btn-default"><?php _e('Read More'); ?></a>
+		</div>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+			<?php sunsetWp_posts_footer() ?>
+	</footer>	
+</article>
