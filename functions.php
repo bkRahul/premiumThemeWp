f1d3ea8490b14907449885d19e72da3171fc3282
bkRahul <rahulsatya1068@gmail.com>
2019-01-09 00:05:09 +0530

    enqueued custom-posts.php

--- functions.php
+++ functions.php
@@ -6,4 +6,5 @@ require get_template_directory(). '/inc/theme-support.php';
 require get_template_directory(). '/inc/custom-posts.php';
 require get_template_directory(). '/inc/cleanup.php';
 require get_template_directory(). '/inc/walker.php';
+require get_template_directory(). '/inc/custom-functions.php';
 ?>
