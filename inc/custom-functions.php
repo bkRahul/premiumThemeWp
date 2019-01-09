0ba8c6dac7cd08b5e2ceb8a13136a9999251b884
bkRahul <rahulsatya1068@gmail.com>
2019-01-09 23:52:20 +0530

    added the posts meta function to display time and categories

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -13,8 +13,20 @@
 //post loop meta custom function 
 function sunsetWp_posts_meta() {
 
-	$posted_on = human_time_diff(get_the_time('U'), current_time('timestamp'));
-	echo '<span class="posted-on">Posted '. $posted_on . ' ago';
+	$posted_on = human_time_diff( get_the_time('U'), current_time('timestamp') );
+	$categories = get_the_category();
+	$seperator = ', ';
+	$output = '';
+	$i = 1;
+
+	if(!empty( $categories )):
+		foreach ($categories as $category):
+	if($i > 1): $output .=$seperator; endif;
+	$output .= '<a href="'. esc_url(get_category_link( $category->term_id)) .'" alt="'. esc_attr( 'View all posts in%s', $category->name ) .'">'. esc_html( $category->name) .'</a>';
+$i++;
+endforeach;
+endif;
+	return '<span class="posted-on">Posted <a href="'.esc_url( get_permalink() ).'">'. $posted_on . '</a> ago / </span><span class="posted-in">'. $output .'</span>';
 }
 
 function sunsetWp_posts_footer() {
