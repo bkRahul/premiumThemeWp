ff8b4be2a27b330b712a4423c1cbb1cfee0c91fc
bkRahul <rahulsatya1068@gmail.com>
2019-01-09 23:55:08 +0530

    added a loop to display posts

--- index.php
+++ index.php
@@ -1,4 +1,30 @@
+<?php 
+/*
+
+
+This is the template for the index.php
+
+@package SunsetWp
+
+
+*/
+?>
 <?php get_header(); ?>
 
+<div id="primary" class="content-area">
+	<main class="site-main" role="main">
+		<div class="container">
+			<?php if( have_posts() ):
+
+				while( have_posts() ): the_post();
+
+					get_template_part( 'template-parts/content', get_post_format() );
+
+				endwhile;
+
+			endif; ?>
+		</div>
+	</main>
+</div>
 
 <?php get_footer(); ?>
