d2b400f3179919a1619f36b3ea8ad806019a931d
bkRahul <rahulsatya1068@gmail.com>
2019-01-15 23:15:47 +0530

    Added custom Image post type

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -13,19 +13,16 @@ This is the template for the image post content
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('sunsetWp-image-post'); ?> >
 
-<?php if( has_post_thumbnail() ): 
-	$featured_image = wp_get_attachment_url( get_post_thumbnail_id (get_the_ID() ) ); ?>
-
-	<header class="entry-header text-center background-image" style="background-image: url(<?php echo $featured_image; ?>)">
+	<header class="entry-header text-center background-image " style="background-image: url(<?php echo featured_image(); ?>)">
 		<?php the_title('<h1 class="entry-title"><a href="'.esc_url( get_permalink() ).'">', '</a></h1>') ?>
 		<div class="entry-meta">
 			<?php echo sunsetWp_posts_meta(); ?>
 		</div><!-- .entry-meta -->
-		<div class="entry-excerpt">
+		<div class="entry-excerpt image-caption">
 			<?php the_excerpt(); ?>
 		</div>		
 	</header>
-<?php endif ?>
+
 
 	<footer class="entry-footer">
 			<?php echo sunsetWp_posts_footer() ?>
