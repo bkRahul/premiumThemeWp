4ecda9e6724412c5012a3f878510dd2afe2b466b
bkRahul <rahulsatya1068@gmail.com>
2019-01-19 23:59:56 +0530

    custom gallery post type

---
+++ template-parts/content-gallery.php
@@ -0,0 +1,42 @@
+<?php 
+/*
+
+
+This is the template for the standard gallery content
+
+@package SunsetWp
+
+
+*/
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class('sunsetWp-gallery-post'); ?> >
+	<header class="entry-header text-center">
+		<?php the_title('<h1 class="entry-title"><a href="'.esc_url( get_permalink() ).'">', '</a></h1>') ?>
+		<div class="entry-meta">
+			<?php echo sunsetWp_posts_meta(); ?>
+		</div><!-- .entry-meta -->
+	</header>
+
+	<div class="entry-content">
+		<?php if( has_post_thumbnail() ): 
+			$featured_image = wp_get_attachment_url( get_post_thumbnail_id (get_the_ID() ) ); ?>
+		<a class="standard-featured-link" href="<?php the_permalink(); ?>">
+			<div class="standard-featured-image background-image" style="background-image: url(<?php echo $featured_image; ?>)">
+			</div>
+		</a>
+		<?php endif ?>
+
+		<div class="entry-excerpt">
+			<?php the_excerpt(); ?>
+		</div>
+
+		<div class="button-container text-center">
+			<a href="<?php the_permalink(); ?>" class="btn btn-sunsetWp"><?php _e('Read More'); ?></a>
+		</div>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+			<?php echo sunsetWp_posts_footer() ?>
+	</footer>	
+</article>
