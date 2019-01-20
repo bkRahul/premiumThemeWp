a7cfcffde20534994fb73873463919e3fc2e1475
bkRahul <rahulsatya1068@gmail.com>
2019-01-20 23:43:58 +0530

    made code change to show images in posts as slider

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -58,20 +58,22 @@ function sunsetWp_posts_footer() {
 
 //post loop background image custom function 
 
-function featured_image() {
+function featured_image( $num = 1 ) {
 	$output = '';
-	if( has_post_thumbnail() ): 
+	if( has_post_thumbnail() && $num == 1 ): 
 		$output = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
 	else:
 		$attachments = get_posts( array( 
 			'post_type' => 'attachment',
-			'posts_per_page' => 1,
+			'posts_per_page' => $num,
 			'post_parent' => get_the_ID()
 		) );
-		if( $attachments ):
+		if( $attachments && $num == 1 ):
 			foreach ( $attachments as $attachment ):
 				$output = wp_get_attachment_url( $attachment->ID );
 			endforeach;
+			elseif ( $attachments && $num > 1 ):
+				$output = $attachments;
 		endif;
 		
 		wp_reset_postdata();
