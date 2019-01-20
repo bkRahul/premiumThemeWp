b4b481c9186368088c20c95628c6cdc5edc72bb4
bkRahul <rahulsatya1068@gmail.com>
2019-01-20 23:41:57 +0530

    Style changes in video post format

--- template-parts/content-video.php
+++ template-parts/content-video.php
@@ -11,25 +11,31 @@ This is the template for the video post content
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('sunsetWp-video-post'); ?> >
-	<header class="entry-header text-center">
+	<header class="entry-header">
 		<div class="embed-responsive embed-responsive-16by9">
 		<?php 
 			echo get_embedded_media_content(array('audio', 'iframe'));
 		?>
 		</div>
+	</header>
+
+	<div class="entry-content text-center">
+
 		<?php the_title('<h1 class="entry-title"><a href="'.esc_url( get_permalink() ).'">', '</a></h1>') ?>
+
 		<div class="entry-meta">
 			<?php echo sunsetWp_posts_meta(); ?>
 		</div><!-- .entry-meta -->
-	</header>
 
-	<div class="entry-content">
 		<?php if( has_post_thumbnail() ): 
+
 			$featured_image = wp_get_attachment_url( get_post_thumbnail_id (get_the_ID() ) ); ?>
+
 		<a class="standard-featured-link" href="<?php the_permalink(); ?>">
 			<div class="standard-featured-image background-image" style="background-image: url(<?php echo $featured_image; ?>)">
 			</div>
 		</a>
+
 		<?php endif ?>
 
 		<div class="entry-excerpt">
