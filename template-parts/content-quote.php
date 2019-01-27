ee6cb1e54855e0a98c91075a16493f153fcfabea
bkRahul <rahulsatya1068@gmail.com>
2019-01-27 23:56:50 +0530

    added quote post type

--- template-parts/content-quote.php
+++ template-parts/content-quote.php
@@ -2,7 +2,7 @@
 /*
 
 
-This is the template for the standard post content
+This is the template for the quote post content
 
 @package SunsetWp
 
@@ -11,12 +11,16 @@ This is the template for the standard post content
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('sunsetWp-quote-post'); ?> >
+
 	<header class="entry-header text-center">
+		<?php the_title('<h1 class="entry-title"><a href="'.esc_url( get_permalink() ).'">', '</a></h1>') ?>
+	</header>
+	<div class="entry-content text-center">
 		<div class="entry-content col-sm-10 offset-sm-1 col-md-8 offset-md-2">
 				<h2 class="quote-text"><?php echo get_the_content(); ?></h2>
 				<h2 class="quote-author"><?php echo get_the_excerpt(); ?></h2>				
 		</div>
-	</header>
+	</div>
  	<footer class="entry-footer">
 		<?php echo sunsetWp_posts_meta(); ?>
 	</footer>	
