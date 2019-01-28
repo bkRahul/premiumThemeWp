2807d69558922ece29b7cf892e99a982da4ff1c8
bkRahul <rahulsatya1068@gmail.com>
2019-01-29 00:00:11 +0530

    changed jquery from slim version as ajax was not supported

--- inc/scripts-enqueue.php
+++ inc/scripts-enqueue.php
@@ -73,7 +73,7 @@ function sunsetWp_script_enqueue() {
 
 	wp_deregister_script('jquery');
 
-	wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', false, '3.2.1', true);
+	wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js', false, '3.3.1', true);
 
 	wp_enqueue_script('jquery');
 	
