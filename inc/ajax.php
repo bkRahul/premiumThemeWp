ee944adbd2ba2b8864f3ae723f7d95b38d6c25ee
bkRahul <rahulsatya1068@gmail.com>
2019-01-28 23:58:12 +0530

    added ajax load more for posts in homepage

--- inc/ajax.php
+++ inc/ajax.php
@@ -8,4 +8,25 @@ This is the file for ajax functions
 
 
 */
+
+add_action('wp_ajax_nopriv_sunsetWp_load_more', 'sunsetWp_load_more');
+add_action('wp_ajax_sunsetWp_load_more', 'sunsetWp_load_more');
+
+function sunsetWp_load_more() {
+	$paged = $_POST['page'];
+	$query = new WP_Query(array(
+		'post_type' => 'post',
+		'paged' => $paged ));
+
+if( $query->have_posts() ):
+
+				while( $query->have_posts() ): $query->the_post();
+
+					get_template_part( 'template-parts/content', get_post_format() );
+
+				endwhile;	
+						endif;	
+		wp_reset_postdata();		
+	die();
+}
 ?>
