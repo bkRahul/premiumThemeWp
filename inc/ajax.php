e37f0054eb9e2542915ae66cc46ea993a114590d
bkRahul <rahulsatya1068@gmail.com>
2019-01-29 23:44:31 +0530

    added query to load more function

--- inc/ajax.php
+++ inc/ajax.php
@@ -13,20 +13,23 @@ add_action('wp_ajax_nopriv_sunsetWp_load_more', 'sunsetWp_load_more');
 add_action('wp_ajax_sunsetWp_load_more', 'sunsetWp_load_more');
 
 function sunsetWp_load_more() {
-	$paged = $_POST['page'];
+	$paged = $_POST['page']+1;
 	$query = new WP_Query(array(
 		'post_type' => 'post',
 		'paged' => $paged ));
 
-if( $query->have_posts() ):
+	if( $query->have_posts() ):
 
-				while( $query->have_posts() ): $query->the_post();
+		while( $query->have_posts() ): $query->the_post();
 
-					get_template_part( 'template-parts/content', get_post_format() );
+			get_template_part( 'template-parts/content', get_post_format() );
 
-				endwhile;	
-						endif;	
-		wp_reset_postdata();		
-	die();
+		endwhile;	
+
+	endif;	
+
+wp_reset_postdata();		
+
+die();
 }
 ?>
