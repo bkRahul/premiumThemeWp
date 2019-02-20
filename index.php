f06c6854fb0a3dd40fe06288b43767b3e3418f6e
bkRahul <rahulsatya1068@gmail.com>
2019-02-20 23:55:14 +0530

    added pagination to home page posts

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ This is the template for the index.php
 		<div class="container sunsetWp-posts-container">
 			<?php if( have_posts() ):
 
-				echo '<div class="page-limit" data-page="http://localhost/sunsetWp/">';
+				echo '<div class="page-limit" data-page="">';
 
 				while( have_posts() ): the_post();
 
@@ -25,13 +25,27 @@ This is the template for the index.php
 				endwhile;
 
 				echo '</div>';
+			?>
+				
+<div class="nav-previous alignleft" style="float: left;"><?php previous_posts_link( 'Older posts' ); ?></div>
+<div class="nav-next alignright" style="float: right;"><?php next_posts_link( 'Newer posts' ); ?></div>
 
-			endif; ?>
+<?php else : ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+
+
+		<?php	endif; ?>
+
 		</div>
 
 		<div class="container text-center">
 			<a class="btn-sunsetWp-load sunsetWp-load-more" data-page="1" data-url="<?php echo admin_url('admin-ajax.php'); ?>"><span class="sunset-icon sunset-loading"> </span>
 			<span class="load-text"> Load More</span></a>
+
+<!-- Add the pagination functions here. -->
+
+
+
 		</div>
 	</main>
 </div>
