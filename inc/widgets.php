3f8e5f6c48a98ef50bdc30b046b3770b7136a71a
bkRahul <rahulsatya1068@gmail.com>
2019-03-29 23:58:09 +0530

    custom widget template

---
+++ inc/widgets.php
@@ -0,0 +1,81 @@
+<?php 
+/*
+
+
+This is the template for the custom widgets
+
+@package SunsetWp
+
+
+*/
+
+
+class sunsetWp_Profile_Widget extends WP_Widget {
+	
+	//setup widget name, description 
+	public function __construct() {
+
+		$widget_ops = array(
+			'classname' => 'sunsetWp-profile-widget',
+			'description' => 'Custom Sunset Profile Widget'
+		);
+
+		parent::__construct( 'sunset_profile', 'Sunset Profile', $widget_ops );
+	}
+
+	//backend display of widget
+	public function form( $instance ) {
+		echo '<p><strong>No Options for this Widget!</strong><br/> You can create fields for this Widget from <a href="./admin.php?page=premium_sunsetWp">this page</a></p>';
+	}
+
+	//frontend display of widget
+	public function widget( $args, $instance) {
+
+		$picture = esc_attr( get_option('profile_picture') );
+		$firstname = esc_attr( get_option('first_name') );
+		$lastname = esc_attr( get_option('last_name') );
+
+		$fullname = $firstname.' '.$lastname;
+		$description = esc_attr( get_option('description') );
+
+		$twitter_icon = esc_attr( get_option('twitter_handler') );
+		$facebook_icon = esc_attr( get_option('facebook_handler') );
+		$googleplus_icon = esc_attr( get_option('gplus_handler') );		
+
+		echo $args['before_widget']; 
+
+?>
+
+<div class="sidebar-preview">
+	<div class="image-container">
+		<div id="profile-image-preview" class="profile_picture" style="background-image: url(<?php print $picture ?>);"></div>
+	</div>
+ 
+
+	<h1 class="admin-name"><?php print $fullname; ?></h1>
+	<h4 class="admin-description"><?php print $description; ?></h4>
+	<div class="admin-social-icons">
+		<?php if( !empty( $facebook_icon ) ): ?>
+			<a href="https://facebook.com/<?php echo $facebook_icon; ?>" target="_blank"><span class="sunset-icon-sidebar sunset-icon sunset-facebook"></span></a>
+		<?php endif; 
+
+		 if( !empty( $googleplus_icon ) ): ?>
+			<a href="https://twitter.com/<?php echo $googleplus_icon; ?>" target="_blank"><span class="sunset-icon-sidebar sunset-icon sunset-googleplus"></span></a>
+		<?php endif; 
+
+		 if( !empty( $twitter_icon ) ): ?>
+			<a href="https://plus.gogle.com/<?php echo $twitter_icon; ?>" target="_blank"><span class="sunset-icon-sidebar sunset-icon sunset-twitter"></span></a>
+		<?php endif; ?>		
+	</div>
+</div>
+
+<?php 
+
+	echo $args['before_widget'];
+
+	}
+}
+
+add_action( 'widgets_init', function(){
+	register_widget( 'sunsetWp_Profile_Widget' );
+} );
