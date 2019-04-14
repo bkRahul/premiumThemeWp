89e7619b019b4f21b41b1cc9cf8b87c94e06c18d
bkRahul <rahulsatya1068@gmail.com>
2019-04-14 23:39:13 +0530

    added footer menu

--- footer.php
+++ footer.php
@@ -10,6 +10,24 @@ This is the template for the footer
 */
 
 ?>
+<footer class="footer-container">
+
+<div class="footer-nav-container">
+<nav class="navbar navbar-expand-lg navbar-light">
+
+<?php wp_nav_menu(array(
+'theme_location'=>'secondary',
+'container'=>false,
+'menu_class'=>'navbar-nav mr-auto',
+'menu_item'=>'nav-item',
+'walker'=> new sunsetWp_Walker_Nav_Primary()	
+)); ?>
+
+
+</nav>
+
+</div><!-- .header-nav -->	
+</footer>
 
 <?php wp_footer(); ?>
 
